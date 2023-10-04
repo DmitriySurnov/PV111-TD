9c6ff68617b27ae66ae8e8a22c4740cc44980e7f
DmitriySurnov <79646813832@yandex.ru>
2023-10-04 16:26:07 +0300

    game update

--- src/app/enemy.test/enemy.test.component.ts
+++ src/app/enemy.test/enemy.test.component.ts
@@ -19,6 +19,15 @@ export class EnemyTestComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     let pageWidth:number = window.innerWidth;
     let pageHeight:number = window.innerHeight;
+    this._x = Math.trunc(Math.random()*pageWidth-35);
+    this._y = Math.trunc(Math.random()*pageHeight-152);
+    if (this._x < 0)
+      this._x =0;
+    if (this._y < 0)
+      this._y =0;
+    this._enemyImg.nativeElement.style.top = `${this._y}px`;
+    this._enemyImg.nativeElement.style.left = `${this._x}px`;
+
 
     setInterval(function (_this:EnemyTestComponent, pageWidth:number, pageHeight:number) {
       if (_this._x != Math.trunc((pageWidth/2)+15)) {
@@ -39,11 +48,34 @@ export class EnemyTestComponent implements AfterViewInit {
         (_this._y == Math.trunc((pageHeight/2)-45))){
         let side = Math.trunc(Math.random()*4);
         console.log("side=", side);
+
+        switch (side){
+          case  0://top
+            _this._x = Math.trunc(Math.random()*pageWidth-35);
+            _this._y = 0;
+            break;
+          case  1://bottom
+            _this._x = Math.trunc(Math.random()*pageWidth-35);
+            _this._y = pageHeight-152;
+            break;
+          case  2://left
+            _this._x = 0;
+            _this._y = Math.trunc(Math.random()*pageHeight-152);
+            break;
+          case  3://right
+            _this._x = pageWidth-35;
+            _this._y = Math.trunc(Math.random()*pageHeight-152);
+            break;
+        }
       }
+      if (_this._x < 0)
+        _this._x =0;
+      if (_this._y < 0)
+        _this._y =0;
 
       _this._enemyImg.nativeElement.style.top = `${_this._y}px`;
       _this._enemyImg.nativeElement.style.left = `${_this._x}px`;
-    },20,this,pageWidth,pageHeight)
+    },10,this,pageWidth,pageHeight)
   }
 
 }
