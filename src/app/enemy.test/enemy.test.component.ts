ff13a44f5abdc75f685827b28fa8cc3ae3f14a1a
DmitriySurnov <79646813832@yandex.ru>
2023-10-04 14:14:06 +0300

    game update

--- src/app/enemy.test/enemy.test.component.ts
+++ src/app/enemy.test/enemy.test.component.ts
@@ -1,11 +1,11 @@
-import {Component, ElementRef, ViewChild, ViewRef} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-enemy-test',
   templateUrl: './enemy.test.component.html',
   styleUrls: ['./enemy.test.component.css']
 })
-export class EnemyTestComponent {
+export class EnemyTestComponent implements AfterViewInit {
   @ViewChild('EnemyImg') _enemyImg: ElementRef;
   public _x:number;
   public _y:number;
@@ -14,6 +14,25 @@ export class EnemyTestComponent {
     this._x = 0;
     this._y = 0;
     this._enemyImg = ViewChild("EnemyImg")
+  }
+
+  ngAfterViewInit(): void {
+    console.log("X=", this._enemyImg.nativeElement.x);
+    console.log("Y=", this._enemyImg.nativeElement.y);
+
+    let pageWidth:number = window.innerWidth;
+    let pageHeight:number = window.innerHeight;
+
+    console.log("pageWidth=", pageWidth);
+    console.log("pageHeight=", pageHeight);
+
+    setInterval(function (_this:EnemyTestComponent, pageWidth:number, pageHeight:number) {
+      if (_this._x < (pageWidth/2)+15)  _this._x+=1;
+      if (_this._y < (pageHeight/2)-45) _this._y+=1;
+
+      _this._enemyImg.nativeElement.style.top = `${_this._y}px`;
+      _this._enemyImg.nativeElement.style.left = `${_this._x}px`;
+    },10,this,pageWidth,pageHeight)
   }
 
 }
